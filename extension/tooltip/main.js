5a33a10b6f58a623b6f8916676cc106b5c30c765
Kevin Hou <kevin.ch.hou@gmail.com>
2018-11-10 14:30:37 -0500

    Working tooltip logic

--- extension/tooltip/main.js
+++ extension/tooltip/main.js
@@ -1,5 +1,32 @@
-$("parsel").hover(function(event) {
-    console.log(event.target);
-})
+$(document).ready(function(){
+    $(".btn-primary").click(function(){
+        $("parsel").tooltip('show');
+    });
+    $(".btn-warning").click(function(){
+        $("parsel").tooltip('hide');
+    });
+    $(".btn-success").click(function(){
+        $("parsel").tooltip('toggle');
+    });
+    $(".btn-danger").click(function(){
+       $("parsel").tooltip('destroy');
+    });
+});
 
-$( "#University" ).tooltip({ content: '<p>Translation</p><img src="https://www.fillmurray.com/200/300" />' });
+$( "parsel" ).tooltip({
+    trigger: "click",
+    html: "true"
+});
+
+var replaceWord = function(word, translation, image) {
+    var markup = []
+    markup.append("<parsel ")
+
+    const size = 30;
+    var tooltipHTML = `<p>${translation}</p><img width='${size}' height='${size}' src='${image}' />`
+    markup.append(`title="${tooltipHTML}"`)
+
+    markup.append(`>${word}</parsel>`)
+
+    return markup.join('')
+}
