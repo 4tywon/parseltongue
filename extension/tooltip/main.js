b78e75c696447442f1f168d940deb994b592c0e5
Kevin Hou <kevin.ch.hou@gmail.com>
2018-11-10 12:37:38 -0500

    Working tooltip

---
+++ extension/tooltip/main.js
@@ -0,0 +1,5 @@
+$("parsel").hover(function(event) {
+    console.log(event.target);
+})
+
+$( "#University" ).tooltip({ content: '<p>Translation</p><img src="https://www.fillmurray.com/200/300" />' });
